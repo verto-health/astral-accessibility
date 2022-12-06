f10096bfa4be036f13bd741de33693782562d831
Cheryl Chen <cheryl.pl.chen@hotmail.com>
2022-12-06 13:11:16 -0500

    chore: added peer dependencies for library

--- projects/astral-accessibility-demo/src/app/app.module.ts
+++ projects/astral-accessibility-demo/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AstralAccessibilityComponent } from 'dist/astral-accessibility';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AstralAccessibilityComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
