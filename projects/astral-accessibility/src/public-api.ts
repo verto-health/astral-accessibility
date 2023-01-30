37cbd161871d3df9943075a95b96ddbb11e3af1c
Aleksander Bodurri <alexbodurri1117@gmail.com>
2023-01-30 13:52:14 -0500

    feat: convert into a Angular Element (#1)
    Allows for use in non-angular sites

--- projects/astral-accessibility/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of astral-accessibility
- */
-
-export * from './lib/astral-accessibility.service';
-export * from './lib/astral-accessibility.component';
-export * from './lib/astral-accessibility.module';
