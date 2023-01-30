a90392a7f6459bdb3efe475c0d689b05629f0dc6
Cheryl Chen <cheryl.pl.chen@hotmail.com>
2023-01-30 15:31:33 -0500

    feat: text size dfs

--- projects/astral-accessibility/src/lib/controls/text-size.component.ts
+++ projects/astral-accessibility/src/lib/controls/text-size.component.ts
@@ -5,7 +5,10 @@ import { Component, inject } from '@angular/core';
   selector: 'astral-text-size',
   standalone: true,
   template: `
-    <button (click)="nextState()" [ngClass]="{ 'in-use': states[currentState] != base }">
+    <button
+      (click)="nextState()"
+      [ngClass]="{ 'in-use': states[currentState] != base }"
+    >
       <div class="title">
         <div class="icon-state-wrap">
           <!-- <i
@@ -22,18 +25,25 @@ import { Component, inject } from '@angular/core';
               active: states[currentState] != base
             }"
           >
-            <svg width="25" height="25" viewBox="0 0 26 21" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              width="25"
+              height="25"
+              viewBox="0 0 26 21"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <defs>
                 <clipPath id="26ttiz3qla">
-                  <path d="M1440 0v900H0V0h1440z"/>
+                  <path d="M1440 0v900H0V0h1440z" />
                 </clipPath>
                 <clipPath id="gg0ujnavhb">
-                  <path d="M20.09 0c.654 0 1.183.522 1.183 1.167V3.24c0 .644-.53 1.167-1.182 1.167a1.174 1.174 0 0 1-1.182-1.167v-.907h-7.09v16.334h3.02c.653 0 1.182.522 1.182 1.166 0 .645-.53 1.167-1.182 1.167H6.434a1.174 1.174 0 0 1-1.182-1.167c0-.644.53-1.166 1.182-1.166h3.02V2.333h-7.09v.907c0 .644-.53 1.167-1.182 1.167A1.174 1.174 0 0 1 0 3.24V1.167C0 .522.53 0 1.182 0H20.09zm4.728 9.683c.653 0 1.182.523 1.182 1.167v1.633c0 .645-.53 1.167-1.182 1.167a1.174 1.174 0 0 1-1.182-1.167v-.466h-1.772v6.65h1.3c.652 0 1.181.522 1.181 1.166 0 .645-.529 1.167-1.181 1.167H18.2a1.174 1.174 0 0 1-1.182-1.167c0-.644.53-1.166 1.182-1.166h1.3v-6.65h-1.773v.466c0 .645-.529 1.167-1.182 1.167a1.174 1.174 0 0 1-1.181-1.167V10.85c0-.644.529-1.167 1.181-1.167h8.273z"/>
+                  <path
+                    d="M20.09 0c.654 0 1.183.522 1.183 1.167V3.24c0 .644-.53 1.167-1.182 1.167a1.174 1.174 0 0 1-1.182-1.167v-.907h-7.09v16.334h3.02c.653 0 1.182.522 1.182 1.166 0 .645-.53 1.167-1.182 1.167H6.434a1.174 1.174 0 0 1-1.182-1.167c0-.644.53-1.166 1.182-1.166h3.02V2.333h-7.09v.907c0 .644-.53 1.167-1.182 1.167A1.174 1.174 0 0 1 0 3.24V1.167C0 .522.53 0 1.182 0H20.09zm4.728 9.683c.653 0 1.182.523 1.182 1.167v1.633c0 .645-.53 1.167-1.182 1.167a1.174 1.174 0 0 1-1.182-1.167v-.466h-1.772v6.65h1.3c.652 0 1.181.522 1.181 1.166 0 .645-.529 1.167-1.181 1.167H18.2a1.174 1.174 0 0 1-1.182-1.167c0-.644.53-1.166 1.182-1.166h1.3v-6.65h-1.773v.466c0 .645-.529 1.167-1.182 1.167a1.174 1.174 0 0 1-1.181-1.167V10.85c0-.644.529-1.167 1.181-1.167h8.273z"
+                  />
                 </clipPath>
               </defs>
               <g clip-path="url(#26ttiz3qla)" transform="translate(-1091 -581)">
                 <g clip-path="url(#gg0ujnavhb)" transform="translate(1091 581)">
-                  <path fill="#FFF" d="M0 0h26v21H0V0z"/>
+                  <path fill="#FFF" d="M0 0h26v21H0V0z" />
                 </g>
               </g>
             </svg>
@@ -41,12 +51,23 @@ import { Component, inject } from '@angular/core';
 
           <div class="state-dots-wrap">
             <span>{{ states[currentState] }}</span>
-            <div class="dots" [ngClass]="{ inactive: states[currentState] === base }">
-              <div class="dot" [ngClass]="{ active: states[currentState] === 'Medium Text' }"></div>
-              <div class="dot" [ngClass]="{ active: states[currentState] === 'Large Text' }"></div>
+            <div
+              class="dots"
+              [ngClass]="{ inactive: states[currentState] === base }"
+            >
               <div
                 class="dot"
-                [ngClass]="{ active: states[currentState] === 'Extra Large Text' }"
+                [ngClass]="{ active: states[currentState] === 'Medium Text' }"
+              ></div>
+              <div
+                class="dot"
+                [ngClass]="{ active: states[currentState] === 'Large Text' }"
+              ></div>
+              <div
+                class="dot"
+                [ngClass]="{
+                  active: states[currentState] === 'Extra Large Text'
+                }"
               ></div>
             </div>
           </div>
@@ -70,6 +91,22 @@ export class TextSizeComponent {
 
   _style: HTMLStyleElement;
 
+  updateTextSize(node: HTMLElement, scale: number) {
+    const children = node.children;
+    console.log(children);
+    if (children.length === 0) {
+      // change font size
+      const currentFontSize = window.getComputedStyle(node).fontSize;
+      const currentFontSizeNum = parseFloat(currentFontSize);
+      node.style.fontSize = `${currentFontSizeNum * scale}px`;
+    } else {
+      // has children, don't change font size and move on
+      // for (const child of children) {
+      //   console.log(child);
+      // }
+    }
+  }
+
   nextState() {
     this.currentState += 1;
     this.currentState = this.currentState % 4;
@@ -82,7 +119,7 @@ export class TextSizeComponent {
     this._style = this.document.createElement('style');
 
     if (this.states[this.currentState] === 'Medium Text') {
-      this.document.documentElement.classList.add('astral_medium_text');
+      this.updateTextSize(document.body, 1.1);
     } else {
       this.document.documentElement.classList.remove('astral_medium_text');
     }
