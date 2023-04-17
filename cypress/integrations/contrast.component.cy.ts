6bed696b1648462f4d29ac2d738c17b16fe1461b
Cheryl <43507623+cherylchen2@users.noreply.github.com>
2023-04-17 16:42:27 -0400

    test: text size test (#5)
    * test: text size test and fix text size component variable name
    
    * chore: style fix
    
    * chore: update ubuntu version, https://github.com/actions/virtual-environments/issues/6002
    
    * chore: update ubuntu version to 20.04
    
    * chore: build app before starting
    
    * chore: update ubuntu version to 20.04
    
    * fix(ci): cypress demo server to run correctly
    
    * chore: remove unused component testing
    
    ---------
    
    Co-authored-by: Cho Yin Yong <choyiny@gmail.com>

--- cypress/integrations/contrast.component.cy.ts
+++
@@ -1,9 +0,0 @@
-import { ContrastComponent } from 'projects/astral-accessibility/src/lib/controls/contrast.component';
-
-describe('My First Test', () => {
-  it('Contrast component', () => {
-    it('mounts', () => {
-      cy.mount(ContrastComponent);
-    });
-  });
-});
